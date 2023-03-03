1fe6aaae66f726e4f7ce2977b6e471b6d8aa7f98
Kyle Territo <kterri3@lsu.edu>
2023-03-03 16:41:55 -0600

    practicing project code

---
+++ che4230_hw/Project Stuff/source_code.py
@@ -0,0 +1,26 @@
+# DATA PREPROCESSING
+#Importing Excel file
+import pandas as pd
+import numpy as np
+import sklearn 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+df = pd.read_excel('Data Plant.xlsx')
+#print(df)
+
+
+# DIMENSIONALITY REDUCTION
+
+
+
+# CLUSTERING
+
+
+
+
+# PLOTTING IMPLEMENTATIONS
