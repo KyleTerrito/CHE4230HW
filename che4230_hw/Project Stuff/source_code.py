19a3e774e1c84d1cde226170694df3572d966160
Kyle Territo <kterri3@lsu.edu>
2023-03-07 19:39:37 -0600

    project

--- che4230_hw/Project Stuff/source_code.py
+++ che4230_hw/Project Stuff/source_code.py
@@ -1,26 +1,68 @@
-# DATA PREPROCESSING
-#Importing Excel file
+# whole_sale.py
+
 import pandas as pd
-import numpy as np
-import sklearn 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 
-df = pd.read_excel('Data Plant.xlsx')
-#print(df)
+df = pd.read_csv('data/Wholesale customers data.csv')
+# print(df.isnull().sum()) # print the sum of null values
 
+df = df.drop(labels=['Channel', 'Region'], axis=1)
+# print(df.head())
 
-# DIMENSIONALITY REDUCTION
+# preprocessing
+T = preprocessing.Normalizer().fit_transform(df)
 
+# change n_clusters to 2, 3 and 4 etc. to see the output patterns
+n_clusters = 3 # number of cluster
 
+# Clustering using KMeans
+kmean_model = KMeans(n_clusters=n_clusters)
+kmean_model.fit(T)
+centroids, labels = kmean_model.cluster_centers_, kmean_model.labels_
+# print(centroids)
+# print(labels)
 
-# CLUSTERING
+# Dimesionality reduction to 2
+pca_model = PCA(n_components=2)
+pca_model.fit(T) # fit the model
+T = pca_model.transform(T) # transform the 'normalized model'
+# transform the 'centroids of KMean'
+centroid_pca = pca_model.transform(centroids)
+# print(centroid_pca)
 
+# colors for plotting
+colors = ['blue', 'red', 'green', 'orange', 'black', 'brown']
+# assign a color to each features (note that we are using features as target)
+features_colors = [ colors[labels[i]] for i in range(len(T)) ]
 
+# plot the PCA components
+plt.scatter(T[:, 0], T[:, 1],
+            c=features_colors, marker='o',
+            alpha=0.4
+        )
 
+# plot the centroids
+plt.scatter(centroid_pca[:, 0], centroid_pca[:, 1],
+            marker='x', s=100,
+            linewidths=3, c=colors
+        )
 
-# PLOTTING IMPLEMENTATIONS
+# store the values of PCA component in variable: for easy writing
+xvector = pca_model.components_[0] * max(T[:,0])
+yvector = pca_model.components_[1] * max(T[:,1])
+columns = df.columns
+
+# plot the 'name of individual features' along with vector length
+for i in range(len(columns)):
+    # plot arrows
+    plt.arrow(0, 0, xvector[i], yvector[i],
+                color='b', width=0.0005,
+                head_width=0.02, alpha=0.75
+            )
+    # plot name of features
+    plt.text(xvector[i], yvector[i], list(columns)[i], color='b', alpha=0.75)
+
+plt.show()
